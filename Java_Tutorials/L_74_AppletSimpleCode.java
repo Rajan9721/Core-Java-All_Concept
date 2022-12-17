50f9d66b58e06d15b3aacf5fc582ed2c21b2c5af
Rajan9721 <rn638600@gmail.com>
2022-12-17 10:25:29 +0530

    add

---
+++ Java_Tutorials/L_74_AppletSimpleCode.java
@@ -0,0 +1,16 @@
+package Java_Tutorials;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class L_74_AppletSimpleCode extends Applet {
+
+    public void paint(Graphics g){
+        g.drawString("Welcome", 150, 150);
+    }
+
+}
+/*
+<applet code = "L_74_AppletSimpleCode.class" width=300 height=300>
+</applet>
+ */

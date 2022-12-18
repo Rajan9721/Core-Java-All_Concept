ade37f452079986dea19fce68779d6a2b82eeab9
Rajan9721 <rn638600@gmail.com>
2022-12-18 11:27:30 +0530

    Add Shift operator program

---
+++ Operator/LeftShift.java
@@ -0,0 +1,10 @@
+package Operator;
+
+public class LeftShift {
+    public static void main(String[] args){
+        System.out.println(15<<2); // 15*2^2 = 15*4 = 60
+        System.out.println(12<<5); // 12*5^5 = 12*3125 = 37500
+        System.out.println(20<<2); // 20*2^2 = 20*4 = 80
+        System.out.println(15<<4);//15*2^4=15*16=240
+    }
+}
